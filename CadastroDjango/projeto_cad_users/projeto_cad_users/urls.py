30846062ed0d613c319a39ad8b02617961ed9480
Marcus Filipi Lehmkuhl <116232636+MarcusLehmkuhl@users.noreply.github.com>
2024-02-27 13:37:05 -0300

    Delete CadastroDjango directory

--- CadastroDjango/projeto_cad_users/projeto_cad_users/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from app_cad_usuarios import views
-urlpatterns = [
-    # rota, view responsável, nome de referência
-    # users.com
-    path('',views.home,name='home'),
-]
